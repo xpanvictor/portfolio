5d5f79ea7171969eb256db6d6c5423761ae3d4de
xpanvictor <xpanvictor@gmail.com>
2022-09-03 18:52:28 +0100

    Fix: meta

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -21,6 +21,7 @@ class MyDocument extends Document {
                     <link href="https://fonts.googleapis.com/css2?family=Calistoga&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Ubuntu:ital,wght@0,300;0,400;0,500;0,700;1,300;1,400;1,500;1,700&display=swap" rel="stylesheet" />
                     <link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/devicons/devicon@v2.15.1/devicon.min.css" />
                     <link rel="icon" href="/favicon.ico" />
+                    <meta name="google-site-verification" content="3VfzJDLeT511d7wHxfZVfoIqrn2l87e1z68u2LbZdaE" />
                 </Head>
                 <body>
                 <Main />
