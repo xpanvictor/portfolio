4376edb4a1d0740af94bda8fe247a9f2d90642b5
Xpan Victor <xpanvictor@gmail.com>
2022-11-21 09:51:12 +0100

    Credits to the freecodecamp radio for their lofi

--- src/components/body.tsx
+++ src/components/body.tsx
@@ -56,9 +56,9 @@ const Body = ({children, className, showMenu, setShowMenu}:
             {/*bottom nav*/}
             <section className='bg-black/20 z-30 md:p-5 grid place-items-center h-16 w-full
             fixed left-0 bottom-0 backdrop-blur-md shadow-rose-100'>
-                <div className='flex flex-col md:flex-row justify-between md:w-4/5' onClick={()=>alert('Danke pink')}>
+                <div className='flex flex-col md:flex-row justify-between md:w-4/5'>
                     <Text className='cursor-copy mx-auto' weight='black' color='text-ascent-200'>
-                        &lt;!-- Get to know me, pink:)<span className={`${styles.blinking} text-bgs-200`}>_</span> --&gt;
+                        &lt;!-- Get to know me! Credits FreeCodeCamp radio --&gt;
                     </Text>
                     <Text weight='black' color='text-ascent-200'>&copy;xpanvictor 2022</Text>
                 </div>
